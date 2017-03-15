7ea8876bda21a39e2213cdabb25a5d4c884f4657
Reaver01 <reaver_01@gmx.com>
2017-03-14 23:21:32 -0500

    cleanup

--- data/main.js
+++ data/main.js
@@ -4,6 +4,7 @@ var area_markers = [],
   korok_markers = [],
   lab_markers = [],
   lynel_markers = [],
+  molduga_markers = [],
   other_markers = [],
   region_markers = [],
   shrine_markers = [],
@@ -13,46 +14,21 @@ var area_markers = [],
   town_markers = [],
   repeatOnXAxis = false,
   minZoomLevel = 3,
-  skull_red = new google.maps.MarkerImage('skull_red.png',
-              new google.maps.Size(32, 32),
-              new google.maps.Point(0, 0),
-              new google.maps.Point(16, 16)),
-  skull_purple = new google.maps.MarkerImage('skull_purple.png',
-                 new google.maps.Size(32, 32),
-                 new google.maps.Point(0, 0),
-                 new google.maps.Point(16, 16)),
-  skull_blue = new google.maps.MarkerImage('skull_blue.png',
-               new google.maps.Size(32, 32),
-               new google.maps.Point(0, 0),
-               new google.maps.Point(16, 16)),
-  icon_tower = new google.maps.MarkerImage('https://vignette3.wikia.nocookie.net/nintendo/images/3/38/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Tower.svg/revision/latest/scale-to-width-down/40?cb=20170308170324&path-prefix=en',
-               new google.maps.Size(40, 40),
-               new google.maps.Point(0, 0),
-               new google.maps.Point(20, 20)),
-  icon_shrine = new google.maps.MarkerImage('http://vignette2.wikia.nocookie.net/nintendo/images/e/ea/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Shrine_%28completed%29.svg/revision/latest/scale-to-width-down/32?cb=20170308170322&path-prefix=en',
-                new google.maps.Size(32, 32),
-                new google.maps.Point(0, 0),
-                new google.maps.Point(16, 16)),
-  icon_village = new google.maps.MarkerImage('https://vignette4.wikia.nocookie.net/nintendo/images/8/8a/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Village.svg/revision/latest/scale-to-width-down/32?cb=20170308170325&path-prefix=en',
-                 new google.maps.Size(32, 32),
-                 new google.maps.Point(0, 0),
-                 new google.maps.Point(16, 16)),
-  icon_lab = new google.maps.MarkerImage('http://vignette3.wikia.nocookie.net/nintendo/images/5/5e/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Tech_Lab.svg/revision/latest/scale-to-width-down/32?cb=20170308170324&path-prefix=en',
-             new google.maps.Size(32, 32),
-             new google.maps.Point(0, 0),
-             new google.maps.Point(16, 16)),
-  icon_stable = new google.maps.MarkerImage('http://vignette1.wikia.nocookie.net/nintendo/images/b/b0/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Stable.svg/revision/latest/scale-to-width-down/32?cb=20170308170323&path-prefix=en',
-                new google.maps.Size(32, 32),
-                new google.maps.Point(0, 0),
-                new google.maps.Point(16, 16)),
-  icon_other = new google.maps.MarkerImage('http://vignette4.wikia.nocookie.net/nintendo/images/7/7b/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Other_landmark.svg/revision/latest/scale-to-width-down/30?cb=20170308170136&path-prefix=en',
-               new google.maps.Size(30, 30),
-               new google.maps.Point(0, 0),
-               new google.maps.Point(15, 15)),
-  icon_korok = new google.maps.MarkerImage('korok.png',
-               new google.maps.Size(32, 32),
-               new google.maps.Point(0, 0),
-               new google.maps.Point(16, 16));
+  icon_korok = new google.maps.MarkerImage('korok.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16));
+  icon_lab = new google.maps.MarkerImage('http://vignette3.wikia.nocookie.net/nintendo/images/5/5e/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Tech_Lab.svg/revision/latest/scale-to-width-down/32?cb=20170308170324&path-prefix=en', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  icon_other = new google.maps.MarkerImage('http://vignette4.wikia.nocookie.net/nintendo/images/7/7b/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Other_landmark.svg/revision/latest/scale-to-width-down/30?cb=20170308170136&path-prefix=en', new google.maps.Size(30, 30), new google.maps.Point(0, 0), new google.maps.Point(15, 15)),
+  icon_shrine = new google.maps.MarkerImage('http://vignette2.wikia.nocookie.net/nintendo/images/e/ea/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Shrine_%28completed%29.svg/revision/latest/scale-to-width-down/32?cb=20170308170322&path-prefix=en', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  icon_stable = new google.maps.MarkerImage('http://vignette1.wikia.nocookie.net/nintendo/images/b/b0/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Stable.svg/revision/latest/scale-to-width-down/32?cb=20170308170323&path-prefix=en', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  icon_tower = new google.maps.MarkerImage('https://vignette3.wikia.nocookie.net/nintendo/images/3/38/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Tower.svg/revision/latest/scale-to-width-down/40?cb=20170308170324&path-prefix=en', new google.maps.Size(40, 40), new google.maps.Point(0, 0), new google.maps.Point(20, 20)),
+  icon_village = new google.maps.MarkerImage('https://vignette4.wikia.nocookie.net/nintendo/images/8/8a/The_Legend_of_Zelda_-_Breath_of_the_Wild_-_Map_icon_-_Village.svg/revision/latest/scale-to-width-down/32?cb=20170308170325&path-prefix=en', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  skull_blue = new google.maps.MarkerImage('skull_blue.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  skull_dark_blue = new google.maps.MarkerImage('skull_dkblue.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  skull_green = new google.maps.MarkerImage('skull_green.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  skull_orange = new google.maps.MarkerImage('skull_orange.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  skull_pink = new google.maps.MarkerImage('skull_pink.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  skull_purple = new google.maps.MarkerImage('skull_purple.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  skull_red = new google.maps.MarkerImage('skull_red.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
+  skull_yellow = new google.maps.MarkerImage('skull_yellow.png', new google.maps.Size(32, 32), new google.maps.Point(0, 0), new google.maps.Point(16, 16)),
 
 function minWindow(elem) {
   var Id = $(elem).attr("id").replace("min-", "options-window-");
@@ -61,19 +37,15 @@ function minWindow(elem) {
 
 function getNormalizedCoord(coord, zoom) {
   if (!repeatOnXAxis) return coord;
-
   var y = coord.y,
     x = coord.x,
     tileRange = 1 << zoom;
-
   if (y < 0 || y >= tileRange) {
     return null;
   }
-
   if (x < 0 || x >= tileRange) {
     x = (x % tileRange + tileRange) % tileRange;
   }
-
   return {
     x: x,
     y: y
@@ -81,8 +53,6 @@ function getNormalizedCoord(coord, zoom) {
 }
 
 window.onload = function() {
-
-  // Define our custom map type
   var customMapType = new google.maps.ImageMapType({
     getTileUrl: function(coord, zoom) {
       var normalizedCoord = getNormalizedCoord(coord, zoom);
@@ -95,34 +65,25 @@ window.onload = function() {
     tileSize: new google.maps.Size(256, 256),
     maxZoom: 7,
     name: 'BotW.Hyrule'
-  });
+  }),
+    myOptions = {
+      center: new google.maps.LatLng(0, 0),
+      zoom: minZoomLevel,
+      minZoom: 0,
+      streetViewControl: false,
+      mapTypeControl: false,
+      mapTypeControlOptions: {
+        mapTypeIds: ["custom"]
+      }
+    },
+    map = new google.maps.Map(document.getElementById('map'), myOptions),
+    strictBounds = new google.maps.LatLngBounds(new google.maps.LatLng(-71.41317683396565, -122.6953125), new google.maps.LatLng(71.52490903732816, 120.9375));
 
-  // Basic options for our map
-  var myOptions = {
-    center: new google.maps.LatLng(0, 0),
-    zoom: minZoomLevel,
-    minZoom: 0,
-    streetViewControl: false,
-    mapTypeControl: false,
-    mapTypeControlOptions: {
-      mapTypeIds: ["custom"]
-    }
-  };
-
-  // Init the map and hook our custom map type to it
-  var map = new google.maps.Map(document.getElementById('map'), myOptions);
   map.mapTypes.set('custom', customMapType);
   map.setMapTypeId('custom');
 
-  var strictBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(-71.41317683396565, -122.6953125),
-    new google.maps.LatLng(71.52490903732816, 120.9375));
-
   google.maps.event.addListener(map, 'dragend', function() {
     if (strictBounds.contains(map.getCenter())) return;
-
-    // We're out of bounds - Move the map back within the bounds
-
     var c = map.getCenter(),
       x = c.lng(),
       y = c.lat(),
@@ -130,12 +91,10 @@ window.onload = function() {
       maxY = strictBounds.getNorthEast().lat(),
       minX = strictBounds.getSouthWest().lng(),
       minY = strictBounds.getSouthWest().lat();
-
     if (x < minX) x = minX;
     if (x > maxX) x = maxX;
     if (y < minY) y = minY;
     if (y > maxY) y = maxY;
-
     map.setCenter(new google.maps.LatLng(y, x));
   });
 
@@ -157,14 +116,11 @@ window.onload = function() {
       for (it in region_markers) {
         region_markers[it].setVisible(false);
       }
-
       markerRegion.repaint();
     } else {
-
       for (var it in region_markers) {
         region_markers[it].setVisible(true);
       }
-
       markerRegion.repaint();
     }
 
@@ -172,13 +128,11 @@ window.onload = function() {
       for (it in area_markers) {
         area_markers[it].setVisible(true);
       }
-
       markerArea.repaint();
     } else {
       for (it in area_markers) {
         area_markers[it].setVisible(false);
       }
-
       markerArea.repaint();
     }
 
@@ -186,14 +140,11 @@ window.onload = function() {
       for (it in area2_markers) {
         area2_markers[it].setVisible(true);
       }
-
       markerArea2.repaint();
     } else {
-
       for (it in area2_markers) {
         area2_markers[it].setVisible(false);
       }
-
       markerArea2.repaint();
     }
 
@@ -201,14 +152,11 @@ window.onload = function() {
       for (it in korok_markers) {
         korok_markers[it].setVisible(true);
       }
-
       markerKorok.repaint();
     } else {
-
       for (it in korok_markers) {
         korok_markers[it].setVisible(false);
       }
-
       markerKorok.repaint();
     }
   });
@@ -217,19 +165,21 @@ window.onload = function() {
   document.getElementById("lynel_count").innerHTML = lynel.length;
   document.getElementById("hinox_count").innerHTML = hinox.length;
   document.getElementById("talus_count").innerHTML = talus.length;
+  document.getElementById("molduga_count").innerHTML = talus.length;
 
-  var markerRegion = new MarkerClusterer(map, region_markers),
-    markerArea = new MarkerClusterer(map, area_markers),
+  var markerArea = new MarkerClusterer(map, area_markers),
     markerArea2 = new MarkerClusterer(map, area2_markers),
-    markerTown = new MarkerClusterer(map, town_markers),
-    markerOther = new MarkerClusterer(map, other_markers),
-    markerLab = new MarkerClusterer(map, lab_markers),
-    markerKorok = new MarkerClusterer(map, korok_markers),
-    markerTower = new MarkerClusterer(map, tower_markers),
-    markerShrine = new MarkerClusterer(map, shrine_markers),
-    markerLynel = new MarkerClusterer(map, lynel_markers),
     markerHinox = new MarkerClusterer(map, hinox_markers),
-    markerTalus = new MarkerClusterer(map, talus_markers);
+    markerKorok = new MarkerClusterer(map, korok_markers),
+    markerLab = new MarkerClusterer(map, lab_markers),
+    markerLynel = new MarkerClusterer(map, lynel_markers),
+    markerMolduga = new MarkerClusterer(map, molduga_markers),
+    markerOther = new MarkerClusterer(map, other_markers),
+    markerRegion = new MarkerClusterer(map, region_markers),
+    markerShrine = new MarkerClusterer(map, shrine_markers),
+    markerTalus = new MarkerClusterer(map, talus_markers),
+    markerTower = new MarkerClusterer(map, tower_markers),
+    markerTown = new MarkerClusterer(map, town_markers);
 
   setMarkers(map);
 
@@ -237,10 +187,7 @@ window.onload = function() {
     for (var i = 0; i < regions.length; i++) {
       var place = regions[i];
       var marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         label: {
           text: place[0],
@@ -255,10 +202,7 @@ window.onload = function() {
     for (i = 0; i < areas.length; i++) {
       place = areas[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         label: {
           text: place[0],
@@ -274,10 +218,7 @@ window.onload = function() {
     for (i = 0; i < areas2.length; i++) {
       place = areas2[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         label: {
           text: place[0],
@@ -293,10 +234,7 @@ window.onload = function() {
     for (i = 0; i < towns.length; i++) {
       place = towns[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: icon_village
       });
@@ -312,10 +250,7 @@ window.onload = function() {
     for (i = 0; i < other.length; i++) {
       place = other[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: icon_other
       });
@@ -331,10 +266,7 @@ window.onload = function() {
     for (i = 0; i < labs.length; i++) {
       place = labs[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: icon_lab
       });
@@ -350,10 +282,7 @@ window.onload = function() {
     for (i = 0; i < stables.length; i++) {
       place = stables[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: icon_stable
       });
@@ -369,10 +298,7 @@ window.onload = function() {
     for (i = 0; i < towers.length; i++) {
       place = towers[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: icon_tower
       });
@@ -388,10 +314,7 @@ window.onload = function() {
     for (i = 0; i < shrines.length; i++) {
       place = shrines[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: icon_shrine
       });
@@ -407,10 +330,7 @@ window.onload = function() {
     for (i = 0; i < koroks.length; i++) {
       place = koroks[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: icon_korok,
         visible: false
@@ -427,10 +347,7 @@ window.onload = function() {
     for (i = 0; i < lynel.length; i++) {
       place = lynel[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: skull_red
       });
@@ -446,10 +363,7 @@ window.onload = function() {
     for (i = 0; i < hinox.length; i++) {
       place = hinox[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: skull_purple
       });
@@ -465,10 +379,7 @@ window.onload = function() {
     for (i = 0; i < talus.length; i++) {
       place = talus[i];
       marker = new google.maps.Marker({
-        position: {
-          lat: place[1],
-          lng: place[2]
-        },
+        position: { lat: place[1], lng: place[2] },
         map: map,
         icon: skull_blue
       });
@@ -479,6 +390,22 @@ window.onload = function() {
         this.info.open(map, this);
       });
       talus_markers.push(marker);
+    }
+
+    for (i = 0; i < molduga.length; i++) {
+      place = molduga[i];
+      marker = new google.maps.Marker({
+        position: { lat: place[1], lng: place[2] },
+        map: map,
+        icon: skull_yellow
+      });
+      marker.info = new google.maps.InfoWindow({
+        content: '<h3>' + place[0] + '</h3><p>' + place[1] + ', ' + place[2] + '</p>'
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        this.info.open(map, this);
+      });
+      molduga_markers.push(marker);
     }
   }
 };
